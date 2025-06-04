0e679c0b543adb0538aaf7db2287f992abbf4717
Danin Na <apps.danin@gmail.com>
2025-06-04 13:32:14 -0700

    ts + eslint

---
+++ apps/_docs/eslint.config.js
@@ -0,0 +1,4 @@
+import { nextJsConfig } from "../../packages/redket-eslint/next"
+
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig
---
+++ apps/_web/eslint.config.js
@@ -0,0 +1,4 @@
+import { nextJsConfig } from "@redket/eslint/next-js"
+
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig
