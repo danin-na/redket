1f354988c8cf37cbb742beb984c3a85d87fd8f9f
Danin Na <apps.danin@gmail.com>
2025-06-06 19:47:32 -0700

    cleanup

--- packages/redket-config/src/eslint/react.js
+++ packages/redket-config/src/eslint/react.js
@@ -1,21 +1,18 @@
-import pluginNext from "@next/eslint-plugin-next"
 import pluginReact from "eslint-plugin-react"
 import pluginReactHooks from "eslint-plugin-react-hooks"
 import globals from "globals"
 import { config as baseConfig } from "./_base.js"
 
-export const nextJsConfig = [
+export const reactConfig = [
   ...baseConfig,
   {
-    plugins: { react: pluginReact, "@next/next": pluginNext, "react-hooks": pluginReactHooks, },
-    languageOptions: { ...pluginReact.configs.flat.recommended.languageOptions, globals: { ...globals.serviceworker, }, },
+    plugins: { react: pluginReact, "react-hooks": pluginReactHooks, },
+    languageOptions: { ...pluginReact.configs.flat.recommended.languageOptions, globals: { ...globals.serviceworker, ...globals.browser, }, },
     settings: { react: { version: "detect" }, },
     rules: {
-      ...pluginReact.configs.flat.recommended.rules,  // React’s flat-config rules
-      ...pluginNext.configs.recommended.rules,  // Next.js core and Core-Web-Vitals rules
-      ...pluginNext.configs["core-web-vitals"].rules,
-      ...pluginReactHooks.configs.recommended.rules,  // React-hooks rules 
-      "react/react-in-jsx-scope": "off", // tweaks
+      ...pluginReact.configs.flat.recommended.rules, // React’s flat-config rules
+      ...pluginReactHooks.configs.recommended.rules, // React Hooks rules
+      "react/react-in-jsx-scope": "off",  // tweaks
       "react/prop-types": "off",
     },
   },
